d3cfce6f9fda5a205155f6b8aceb4e6572050010
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-11-18 22:32:45 -0700

    Refactor DocumentProcessor to enhance statement ingestion and filtering logic
    - Introduced a new method to process ItemDocument statements, categorizing them into Edges and Claims based on defined criteria.
    - Implemented filtering for exclusionary statements and irrelevant properties to streamline data processing.
    - Updated documentation to clarify the purpose and functionality of the new methods and their roles in graph processing.

--- src/main/java/edu/velvet/Wikiverse/api/services/wikidata/DocumentProcessor.java
+++ src/main/java/edu/velvet/Wikiverse/api/services/wikidata/DocumentProcessor.java
@@ -1,27 +1,20 @@
 package edu.velvet.Wikiverse.api.services.wikidata;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.Iterator;
+import edu.velvet.Wikiverse.api.models.core.Claim;
+import edu.velvet.Wikiverse.api.models.core.Edge;
+import edu.velvet.Wikiverse.api.models.core.SearchResult;
+import edu.velvet.Wikiverse.api.models.core.Vertex;
+import edu.velvet.Wikiverse.api.services.logging.WikidataDocumentLogger;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Service;
 import org.wikidata.wdtk.datamodel.interfaces.EntityDocument;
 import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
-import org.wikidata.wdtk.datamodel.interfaces.Reference;
-import org.wikidata.wdtk.datamodel.interfaces.SiteLink;
-import org.wikidata.wdtk.datamodel.interfaces.SnakGroup;
 import org.wikidata.wdtk.datamodel.interfaces.Statement;
-import org.wikidata.wdtk.datamodel.interfaces.StatementDocument;
 import org.wikidata.wdtk.wikibaseapi.WbSearchEntitiesResult;
-
-import edu.velvet.Wikiverse.api.models.core.Claim;
-import edu.velvet.Wikiverse.api.models.core.SearchResult;
-import edu.velvet.Wikiverse.api.models.core.Vertex;
-import edu.velvet.Wikiverse.api.models.requests.GraphsetRequest;
-import edu.velvet.Wikiverse.api.services.logging.WikidataDocumentLogger;
 
 /**
  * Service responsible for processing and transforming Wikidata search results
@@ -92,93 +85,139 @@ public class DocumentProcessor {
 	 */
 	public List<SearchResult> ingestWikidataSearchResults(List<WbSearchEntitiesResult> results) {
 		return logger
-				.log(SOURCE + ".ingestWikidataSearchResults()", results)
-				.stream()
-				.map(result -> new SearchResult(result))
-				.collect(Collectors.toList());
+			.log(SOURCE + ".ingestWikidataSearchResults()", results)
+			.stream()
+			.map(result -> new SearchResult(result))
+			.collect(Collectors.toList());
 	}
 
-	public void ingestInitialGraphsetDocument(
-			EntityDocument doc,
-			GraphsetRequest request) {
-		logger.log(SOURCE + ".ingestInitialGraphsetDocument(" + doc.getEntityId().getId() + ")", doc);
+	public Tuple2<List<Edge>, List<Claim>> ingestDocumentsStatements(ItemDocument doc) {
+		logger.log(SOURCE + ".ingestDocumentsStatements(" + doc.getEntityId().getId() + ")", doc);
 
-		Vertex origin = new Vertex();
+		List<Edge> edges = new ArrayList<>();
+		List<Claim> claims = new ArrayList<>();
 
-		String lang = request.getMetadata().getWikiLangTarget();
-		if (doc instanceof ItemDocument iDoc) {
-			origin.setId(doc.getEntityId().getId());
-			origin.setDescription(iDoc.findDescription(lang));
-			origin.setLabel(iDoc.findLabel(lang));
-			Map<String, SiteLink> links = iDoc.getSiteLinks();
-			SiteLink link = links.get(lang + "wiki");
-			String pageTitle = link.getPageTitle();
-			pageTitle = pageTitle.replace(" ", "_").replace("%3A", ":").replace("%2F", "/");
-			String encodedTitle;
-			try {
-				encodedTitle = URLEncoder.encode(pageTitle, "utf-8");
-			} catch (UnsupportedEncodingException e) {
-				throw new RuntimeException("UTF-8 Encoding is not supported in this RunTime", e);
-			}
-			String PROTOCOL = "https://";
-			String SITE_MAIN = ".wikipedia.org/wiki/";
-			origin.setUrl(PROTOCOL + lang + SITE_MAIN + encodedTitle);
+		// Iterate over all of the Statements and Check for any which qualify as Edges
+		doc
+			.getAllStatements()
+			.forEachRemaining((Statement statement) -> {
+				// * Check for Exclusionary Statement Details (null/external-id/monolingual)
+				if (this.statementDefinesExclusionaryInfo(statement)) {
+					return;
+				}
+
+				// * Check for properties present which we don't care about
+				if (this.statementIncludesFilteredProperty(statement)) {
+					return;
+				}
+
+				// * Statement Defines Relevant Data, Create Edges -or- Claims w/ Statement
+				WikidataSnak mainSnak = statement.getMainSnak().accept(new WikidataSnak());
+				if (mainSnak.hasWikidataEntityTarget()) {
+					// Create MainSank Edge and add to List
+					edges.add(new Edge(statement, mainSnak));
+
+					// Check for QualifiedSnakEdge and also add to list
+					statement
+						.getQualifiers()
+						.forEach(group -> {
+							group
+								.getSnaks()
+								.forEach(snak -> {
+									WikidataSnak qualifiedSnak = snak.accept(new WikidataSnak());
+									if (qualifiedSnak.hasWikidataEntityTarget()) {
+										edges.add(new Edge(statement, mainSnak, qualifiedSnak));
+									}
+									// TODO: Handle qualified non-edge Claims?
+								});
+						});
+				} else {
+					// Statement outlines a non-target Claim, not an Edge, save for Claim details.
+					claims.add(new Claim(statement, mainSnak));
+				}
+			});
+
+		return Tuple.of(edges, claims);
+	}
+
+	// !PRIVATE ============================================================>
+	// !PRIVATE ============================================================>
+
+	/**
+	 * Processes an {@link EntityDocument} to initialize and ingest a {@link Vertex}
+	 * into the graphset.
+	 * <p>
+	 * This method extracts relevant information from the given entity document,
+	 * such as the entity's label,
+	 * description, and Wikipedia URL based on a target language. It iterates
+	 * through all statements of the document,
+	 * excluding those that match exclusionary or filtered criteria, and processes
+	 * each statement to determine whether
+	 * it represents edge data or property claim data. Claims are added as
+	 * attributes to the vertex, while edge data
+	 * is handled separately (see implementation for extension).
+	 * </p>
+	 * <p>
+	 * Once the vertex and its associated claims are constructed, the vertex is
+	 * added to the {@code GraphsetRequest}'s graphset.
+	 * </p>
+	 *
+	 * @param doc     the Wikidata entity document containing statements and entity
+	 *                info to ingest (must not be null)
+	 * @param request the graphset request context, including metadata such as
+	 *                language and the graphset to update (must not be null)
+	 */
+	private boolean statementDefinesExclusionaryInfo(Statement statement) {
+		WikidataSnak mainSnak = statement.getMainSnak().accept(new WikidataSnak());
+
+		/**
+		 * ? mainSnak has a null value for one of: datatype, property, value
+		 *
+		 * ? Do not include for missing some component of data
+		 */
+		if (mainSnak.isNull()) {
+			return true;
 		}
 
-		// Statements
-		if (doc instanceof StatementDocument sDoc) {
-			Iterator<Statement> statements = sDoc.getAllStatements();
-			while (statements.hasNext()) {
-				// Set subject/establish claim
-				Claim claim = new Claim();
-
-				// Sets main Snak()
-				Statement statement = statements.next();
-				WikidataSnak main = statement.getMainSnak().accept(new WikidataSnak());
-				claim.setMain(main);
-
-				if (main.getDatatype() != null && main.getDatatype().equals("external-id")) {
-					// Ignore any external-id snaks... skip to next
-					continue;
-				}
-
-				if (main.getDatatype() != null && main.getDatatype().equals("monolingualtext")) {
-					// Ignore monolingual text restatements of properties...
-					continue;
-				}
-
-				if (main.getProperty() != null && main.getProperty().getValue().equals("P8687")) {
-					// Ignore social media followers, data is almost always very stale...
-					continue;
-				}
-
-				// Set Qualifiers
-				List<SnakGroup> qualifiers = statement.getQualifiers();
-				for (SnakGroup group : qualifiers) {
-					WikidataSnakGroup qualifier = new WikidataSnakGroup(group);
-					if (!qualifier.getSnaks().isEmpty()) {
-						claim.addQualifier(qualifier);
-					}
-				}
-
-				// Set References
-				List<Reference> references = statement.getReferences();
-				for (Reference reference : references) {
-					WikidataReference ref = new WikidataReference();
-					for (SnakGroup group : reference.getSnakGroups()) {
-						WikidataSnakGroup refData = new WikidataSnakGroup(group);
-						if (!refData.getSnaks().isEmpty()) {
-							ref.addSnakGroup(refData);
-						}
-					}
-					claim.addReference(ref);
-				}
-
-				// Add claim back to the initial Vertex
-				origin.addClaim(claim);
-			}
+		/**
+		 * ? mainSnak points to an external source/link which is hard to decipher/use
+		 *
+		 * ? Do not include for exteranl sourcing/pathing
+		 */
+		if (mainSnak.getDatatype().equals("external-id")) {
+			return true;
 		}
+		/**
+		 * ? mainSnak is a 'monolingual value' which is typically a value containing
+		 * ? repetitive data such as the label/description
+		 * ? Users select a language target so these can be safely ignored
+		 *
+		 * ? Do not include for repetitive information
+		 */
+		return mainSnak.getDatatype().equals("monolingualtext");
+	}
 
-		request.getGraphset().addVertex(origin);
+	/**
+	 * Checks whether the given statement includes a property that should be
+	 * filtered out
+	 * from further processing. This filtering is based on a predefined list of
+	 * property
+	 * IDs that are considered irrelevant or unsuitable for inclusion in the
+	 * application's data model.
+	 *
+	 * @param statement the Wikidata Statement to check for filtered properties
+	 * @return true if the statement's main property matches a filtered property ID,
+	 *         false otherwise
+	 */
+	private boolean statementIncludesFilteredProperty(Statement statement) {
+		WikidataSnak mainSnak = statement.getMainSnak().accept(new WikidataSnak());
+		List<String> filteredProperties = List.of(
+			"P8687", // SOCIAL MEDIA FOLLOWERS
+			"P9352", // PORTRAIT ARCHIVE ID / NON_EXTERNAL ID MASQUERADING
+			"P935", // COMMONS GALLERY TITLE
+			"P373", // WIKIMEDIA COMMONS CATEGORY
+			"P1343" // DESCRIBED BY SOURCE
+		);
+		return filteredProperties.contains(mainSnak.getProperty().getValue());
 	}
 }
