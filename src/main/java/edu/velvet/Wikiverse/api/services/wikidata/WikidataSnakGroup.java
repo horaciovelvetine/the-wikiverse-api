5e7a0c8207d2fe5e8b3307e6644e095b03f55a0a
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2025-11-18 22:31:22 -0700

    Enhance WikidataSnakGroup with improved constructor and filtering logic
    - Added a default constructor for JSON deserialization.
    - Introduced a new constructor that transforms a SnakGroup into a list of WikidataSnak objects, filtering out null snaks and those with "external-id" and "monolingualtext" datatypes.
    - Updated documentation to clarify the purpose and functionality of the new constructor.

--- src/main/java/edu/velvet/Wikiverse/api/services/wikidata/WikidataSnakGroup.java
+++ src/main/java/edu/velvet/Wikiverse/api/services/wikidata/WikidataSnakGroup.java
@@ -1,11 +1,9 @@
 package edu.velvet.Wikiverse.api.services.wikidata;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.wikidata.wdtk.datamodel.interfaces.SnakGroup;
-
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class WikidataSnakGroup {
@@ -15,6 +13,30 @@ public class WikidataSnakGroup {
 
 	private WikidataValue property = null;
 
+	/**
+	 * Default constructor for JSON deserialization.
+	 */
+	public WikidataSnakGroup() {}
+
+	/**
+	 * Constructs a WikidataSnakGroup from a given SnakGroup.
+	 *
+	 * <p>
+	 * This constructor takes a {@link SnakGroup} from the Wikidata Toolkit API and
+	 * transforms its list of Snaks into
+	 * a list of {@link WikidataSnak} objects. Null snaks and snaks with a datatype
+	 * of "external-id" are filtered out,
+	 * as external-ids are typically not relevant for further processing within the
+	 * application.
+	 *
+	 * <p>
+	 * If the group contains more than one snak after filtering, the property
+	 * information from the original SnakGroup
+	 * is also converted to a {@link WikidataValue} and assigned to this instance.
+	 *
+	 * @param group the original SnakGroup to extract snaks and property from, must
+	 *              not be null
+	 */
 	public WikidataSnakGroup(SnakGroup group) {
 		this.snaks = group.getSnaks() == null
 			? List.of()
@@ -22,7 +44,13 @@ public class WikidataSnakGroup {
 				.getSnaks()
 				.stream()
 				.map(snak -> snak != null ? snak.accept(new WikidataSnak()) : null)
-				.filter(snak -> snak != null && !"external-id".equals(snak.getDatatype()))
+				// Compare to string in case snak.getDatatype() is null
+				.filter(
+					snak ->
+						snak != null &&
+						!"external-id".equals(snak.getDatatype()) &&
+						!"monolingualtext".equals(snak.getDatatype())
+				)
 				.collect(Collectors.toList());
 
 		if (this.snaks.size() > 1) {
